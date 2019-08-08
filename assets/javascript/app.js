aabb4f4434556959a963d458c48aaa6b2652b4e3
brandnn <brandnn@uw.edu>
2019-08-08 11:24:57 -0700

    added js logic for firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,3 +10,37 @@ var aTrainDestination = $("#train-destination");
 var aTrainTime = $("#train-time")
 var aTimeFreq = $("#time-freq")
 
+
+var config = {
+    apiKey: "AIzaSyBg6IyWj3curcfPuAeV8MMy07PgO1WuTjY",
+    authDomain: "first-project-acab1.firebaseapp.com",
+    databaseURL: "https://first-project-acab1.firebaseio.com",
+    projectId: "first-project-acab1",
+    storageBucket: "first-project-acab1.appspot.com",
+    messagingSenderId: "1027238424289",
+    appId: "1:1027238424289:web:0cc4354c688c9a31"
+    };
+    firebase.initializeApp(config);  
+    var database = firebase.database();
+    database.ref("/trains").on("child_added", function(snapshot) {
+
+        //  create local variables to store the data from firebase
+        var trainDiff = 0;
+        var trainRemainder = 0;
+        var minutesTillArrival = "";
+        var nextTrainTime = "";
+        var frequency = snapshot.val().frequency;
+
+        trainDiff = moment().diff(moment.unix(snapshot.val().time), "minutes");
+        trainRemainder = trainDiff % frequency;
+        minutesTillArrival = frequency - trainRemainder;
+        nextTrainTime = moment().add(minutesTillArrival, "m").format("hh:mm A");
+        $("#table-data").append(
+            "<tr><td>" + snapshot.val().name + "</td>" +
+            "<td>" + snapshot.val().destination + "</td>" +
+            "<td>" + frequency + "</td>" +
+            "<td>" + minutesTillArrival + "</td>" +
+            "<td>" + nextTrainTime + "  " + "<a><span class='glyphicon glyphicon-remove icon-hidden' aria-hidden='true'></span></a>" + "</td></tr>"
+        );
+        
+    });
